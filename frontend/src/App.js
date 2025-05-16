c843544733e9b985b3200647aca1bb09642a4b66
My Name <anshul@chelapurath.com>
2025-05-16 17:40:54 -0400

    added file upload feature

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import { createRef } from 'react';
+import './App.css';
+
+function App() {
+  const fileInput = createRef();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.set("receipt", fileInput.current.files[0]);
+
+    try {
+      const response = await fetch('/profile', {
+        method: "POST",
+        body: formData
+      });
+      const parsedResponse = await response.json();
+      if (response.ok){
+        alert("File uploaded");
+      }
+      else{
+        console.error("Some error occurred. Try again")
+      }
+    } catch (e){
+      console.error(e.message)
+    }
+
+  }
+  return (
+    <div className="App">
+    <form onSubmit={onSubmit}>
+        <input type="file" name="receipt" ref={fileInput} />
+        <input type="submit" value="Submit" /> 
+    </form>
+    </div>
+  );
+}
+
+export default App;
